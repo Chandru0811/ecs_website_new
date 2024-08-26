2fc76a705b619720f53984bc8c86521fb7cd1aad
Chandru0811 <chandru08112000@gmail.com>
2024-08-26 12:14:05 +0530

    required links added

---
+++ assets/js/custom.js
@@ -0,0 +1,41 @@
+// console.log("Hello World !!!")
+
+//  $(document).ready(function () {
+//    alert("jQuery is working!");
+//    $("h1").css("color", "green");
+//  });
+
+// $(document).ready(function () {
+//   $("#testForm").validate({
+//     rules: {
+//       name: {
+//         required: true,
+//         minlength: 2,
+//       },
+//       email: {
+//         required: true,
+//         email: true,
+//       },
+//     },
+//     messages: {
+//       name: {
+//         required: "Please enter your name",
+//         minlength: "Your name must be at least 2 characters long",
+//       },
+//       email: {
+//         required: "Please enter your email",
+//         email: "Please enter a valid email address",
+//       },
+//     },
+//     submitHandler: function (form) {
+//       // Print form values to the console
+//       var name = $("#name").val();
+//       var email = $("#email").val();
+//       console.log("Name: " + name);
+//       console.log("Email: " + email);
+
+//       // Optionally submit the form
+//       // form.submit();
+//     },
+//   });
+// });
